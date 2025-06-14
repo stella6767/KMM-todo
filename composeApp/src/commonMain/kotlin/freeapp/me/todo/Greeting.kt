a9b371fc3651aaf2e87d1fd615b3203c81520bb1
stella6767 <stella@6767>
2025-06-14 14:18:35 +0900

    [add] project setup

---
+++ composeApp/src/commonMain/kotlin/freeapp/me/todo/Greeting.kt
@@ -0,0 +1,9 @@
+package freeapp.me.todo
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/freeapp/me/todo/MainViewController.kt
@@ -0,0 +1,5 @@
+package freeapp.me.todo
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
